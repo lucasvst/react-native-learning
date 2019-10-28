edf0815f9f2f66769403fb9875eab60a10381a85
Lucas Teles <lucasvst@gmail.com>
2019-10-27 22:31:03 -0300

    updates voucher data loader

--- src/screens/voucher-data-loader/VoucherDataLoader.js
+++ src/screens/voucher-data-loader/VoucherDataLoader.js
@@ -12,21 +12,32 @@ class VoucherDataLoader extends Component {
                 name: undefined,
                 cpf: undefined,
                 email: undefined,
+            },
+            operationRequest: {
+                vehicleModel: undefined,
+                vehicleModelYear: undefined,
+                vechicleManufactureYear: undefined,
+                vehicleMake: undefined,
+                vehicleValue: undefined,
+                installments: undefined,
             }
         }
     }
     render() {
         return (
             <View style={styles.container}>
-                <Container>
-                    <Header />
-                        {this.state.loading ? <Spinner /> : null}
-                        <View style={styles.content}>
-                            <Text>Nome: {this.state.client.name}</Text>
-                            <Text>CPF: {this.state.client.cpf}</Text>
-                            <Text>email: {this.state.client.email}</Text>
-                        </View>
-                </Container>
+                {this.state.loading ? <Spinner /> : null}
+                <Text>Nome: {this.state.client.name}</Text>
+                <Text>CPF: {this.state.client.cpf}</Text>
+                <Text>email: {this.state.client.email}</Text>
+                <Text>Fabricante: {this.state.operationRequest.vehicleMake}</Text>
+                <Text>Veículo: {this.state.operationRequest.vehicleModel}</Text>
+                <Text>
+                    Ano/Modelo: {this.state.operationRequest.vehicleManufactureYear} /
+                    {this.state.operationRequest.vehicleModelYear}
+                </Text>
+                <Text>Valor: {this.state.operationRequest.vehicleValue}</Text>
+                <Text>Parcelas: {this.state.operationRequest.installments}</Text>
             </View>
         )
     }
@@ -34,7 +45,10 @@ class VoucherDataLoader extends Component {
         this.setState({ loading: true })
         VoucherService.getVoucherData(this.props.navigation.getParam('transactionId')).then(transaction => {
             this.setState({ loading: false })
-            this.setState({ client: transaction.financeTransaction.client })
+            this.setState({
+                client: transaction.financeTransaction.client,
+                operationRequest: transaction.financeTransaction.operationRequest,
+            })
         })
     }
 }
@@ -42,12 +56,9 @@ class VoucherDataLoader extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },
-    content: {
-        flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
 })
 
 export default VoucherDataLoader
